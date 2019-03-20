30e5458cdd1d3cc09951497dbde378e4c19e7c8f
Ferdi Öztürk <foerdi@gmail.com>
2019-03-20 13:05:35 +0100

    Update New-PSDCImage.ps1

--- PSDatabaseClone/functions/image/New-PSDCImage.ps1
+++ PSDatabaseClone/functions/image/New-PSDCImage.ps1
@@ -223,6 +223,17 @@
 
         # Setup the computer object
         $computer = [PsfComputer]$uriHost
+        if (-not ($computer.PSobject.Properties.name -match "IsLocalhost")) {
+            if ($($env:ComputerName).toLower() -eq $($computer.ComputerName).toLower() ) {
+                Add-Member -InputObject $computer -TypeName IsLocalhost -NotePropertyValue $true -NotePropertyName "IsLocalhost"
+                Write-Host $computer.IsLocalhost
+            }
+            else
+            {
+                Add-Member -InputObject $computer -TypeName IsLocalhost -NotePropertyValue $false -NotePropertyName "IsLocalhost"
+                Write-Host $computer.IsLocalhost
+            }
+        }
 
         if (-not $computer.IsLocalhost) {
             # Get the result for the remote test
